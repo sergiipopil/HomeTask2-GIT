f73b3462cbdd96fd6f04aeffd511254ae265a1ac
Serhii Popil <sergii.popil@gmail.com>
2023-09-05 14:49:21 +0300

    add Subtraction method to Calculator class

--- Program.cs
+++ Program.cs
@@ -3,4 +3,6 @@ using HomeTask2_GIT.Classes;
 
 
 var sum = new Calculator(2, 3).Sum();
-Console.Write($"2+3={sum}");
+var subs = new Calculator(12, 3).Subtraction();
+Console.WriteLine($"2+3={sum}");
+Console.WriteLine($"12-3={subs}");
